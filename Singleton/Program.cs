38add0ec11992edafd5465ec669731c0e4b66c57
Alex Holenko <cat-begemot@live.com>
2019-03-01 12:12:49 +0200

    Added Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Singleton
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+			Singleton s1 = Singleton.GetInstance();
+			Singleton s2 = Singleton.GetInstance();
+
+			if(s1.Equals(s2))
+				Console.WriteLine("All ok, both variables are equal");
+			else
+				Console.WriteLine("Hm... They doesn't equal...");
+
+			if(Object.ReferenceEquals(s1,s2))
+				Console.WriteLine("All ok, both variables have got the same reference");
+			else
+				Console.WriteLine("Hm... There are different references...");
+    }
+  }
+
+	public class Singleton
+	{
+		private static Singleton _nstance;
+		private static Object _lock = new Object();
+
+		public static Singleton GetInstance()
+		{
+			lock(_lock)
+			{
+				return _nstance ?? (_nstance = new Singleton());
+			}
+		}
+	}
+}
